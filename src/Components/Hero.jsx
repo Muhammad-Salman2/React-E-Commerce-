fbdaa430e842fce98f5647481719ffea8f14ae5e
salman <salman7official7@gmail.com>
2024-11-11 00:29:50 +0500

    code updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,20 @@
 import { FaAngleRight } from "react-icons/fa6";
-i
+import iphonelogo from '../assets/apple logo.png';
+import { FaArrowRight } from "react-icons/fa6";
 
 
 export default function Hero() {
   return (
     <>
-    <div className="li-hero-main">
+      <div className="li-hero-main">
         <div className="hero-list">
           <ul>
-            <li>Woman’s Fashion <FaAngleRight /></li>
-            <li>Men’s Fashion <FaAngleRight /></li>
+            <li>
+              Woman’s Fashion <FaAngleRight />
+            </li>
+            <li>
+              Men’s Fashion <FaAngleRight />
+            </li>
             <li>Electronics</li>
             <li>Home & Lifestyle</li>
             <li>Medicine</li>
@@ -21,10 +26,20 @@ export default function Hero() {
         </div>
         <div className="center-line">
           <div className="slider">
-            <div>
-              
+            <div className="slider-left">
+              <span className="iphone-logo">
+             <p> iPhone 14 Series</p>
+              </span>
+              <span className="discount-para">
+                <h1>Up to 10% off Voucher</h1>
+              </span>
+              <span className="shop-now">
+                <p>Shop Now <FaArrowRight className="arrow"/></p>
+              </span>
             </div>
+            <div className="slider-right">
 
+            </div>
           </div>
         </div>
       </div>
